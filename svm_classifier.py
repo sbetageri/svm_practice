8e8f642da1b2375fa66dbeb8f7a6bb25dfd256b7
Srirakshith Betageri <sbetageri111@gmail.com>
2017-07-01 06:19:03 +0530

    Refactor to parse data using commas as delim

--- svm_classifier.py
+++ svm_classifier.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-dataframe = pd.read_csv('Skin_NonSkin.txt', delimiter='\t')
+dataframe = pd.read_csv('Skin_NonSkin.txt')
 print(dataframe.head(5))
 
 print('Labels')
